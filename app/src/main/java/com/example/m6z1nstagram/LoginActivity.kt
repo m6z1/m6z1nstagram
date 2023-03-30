ac15e96e6a8b0f8b33fd6b763f58c5504551830a
m6z1 <sonmyeongjiz@dongyang.ac.kr>
2023-03-30 15:25:47 +0900

    add : 파이어베이스 연동

--- app/src/main/java/com/example/m6z1nstagram/LoginActivity.kt
+++ app/src/main/java/com/example/m6z1nstagram/LoginActivity.kt
@@ -2,10 +2,20 @@ package com.example.m6z1nstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.m6z1nstagram.databinding.ActivityLoginBinding
+import com.example.m6z1nstagram.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
+    private val binding = ActivityLoginBinding.inflate(layoutInflater);
+    override fun setContentView(layoutResID: Int) {
+        (binding.root)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
     }
+
 }
