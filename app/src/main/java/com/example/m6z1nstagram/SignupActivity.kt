5e586a3a7b406547b4b3af39a0966d3b3f5d3691
m6z1 <sonmyeongjiz@dongyang.ac.kr>
2023-04-14 00:09:37 +0900

    Feat : ViewBinding

--- app/src/main/java/com/example/m6z1nstagram/SignupActivity.kt
+++ app/src/main/java/com/example/m6z1nstagram/SignupActivity.kt
@@ -2,10 +2,14 @@ package com.example.m6z1nstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.m6z1nstagram.databinding.ActivityLoginBinding
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
